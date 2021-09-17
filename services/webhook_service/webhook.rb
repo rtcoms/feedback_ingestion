fe7b22eef894a774201184647a257cc3538410b3
Rohit Trivedi <86.rohit@gmail.com>
2021-09-17 16:37:01 +0530

    webhook models

---
+++ services/webhook_service/webhook.rb
@@ -0,0 +1,3 @@
+class Webhook
+  attr_accessor :id, :feedback_source
+end
---
+++ services/webhook_service/webhook_event.rb
@@ -0,0 +1,5 @@
+# check duplications
+
+class WebhookEvent
+  attr_accessor :id, :webhook_id, :feedback_data, :customer_info, :feedback_stream_info
+end
